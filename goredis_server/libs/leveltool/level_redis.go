c654fbbeeeaa4aa82228488f0eebe3bf6dc5dc9e
latermoon <latermoon@qq.com>
2013-11-30 01:48:27 +0800

    merge const

--- goredis_server/libs/leveltool/level_redis.go
+++ goredis_server/libs/leveltool/level_redis.go
@@ -2,6 +2,29 @@ package leveltool
 
 /*
 基于leveldb实现的redis持久化层
+
+string
+	+[name]string = "latermoon"
+hash
+	+[info]hash = ""
+	_h[info]name = "latermoon"
+	_h[info]age = "27"
+	_h[info]sex = "M"
+list
+	+[list]list = "0,1"
+	_l[list]#0 = "a"
+	_l[list]#1 = "b"
+	_l[list]#2 = "c"
+	_l[list]#3 = "d"
+zset
+	+[user_rank]zset = "2"
+	_z[user_rank]s#1002#100422 = ""
+	_z[user_rank]s#1006#100423 = ""
+	_z[user_rank]s#10102#300000 = ""
+	_z[user_rank]m#100422 = "1002"
+	_z[user_rank]m#100423 = "1006"
+	_z[user_rank]m#300000 = "10102"
+
 */
 
 import (
@@ -11,6 +34,31 @@ import (
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/syndtr/goleveldb/leveldb/opt"
 	"sync"
+)
+
+// 共用字段
+const (
+	SEP        = "#"
+	SEP_LEFT   = "["
+	SEP_RIGHT  = "]"
+	KEY_PREFIX = "+"
+)
+
+// 数据结构的key后缀
+const (
+	STRING_SUFFIX = "string"
+	HASH_SUFFIX   = "hash"
+	LIST_SUFFIX   = "list"
+	SET_SUFFIX    = "set"
+	ZSET_SUFFIX   = "zset"
+)
+
+// 数据结构的key前缀
+const (
+	HASH_PREFIX = "_h"
+	LIST_PREFIX = "_l"
+	SET_PREFIX  = "_s"
+	ZSET_PREFIX = "_z"
 )
 
 type IteratorDirection int
