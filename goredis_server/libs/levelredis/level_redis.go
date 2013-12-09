5f86735373f1190ea9c0872556ed042c356c694c
latermoon <latermoon@qq.com>
2013-12-09 19:31:02 +0800

    up

--- goredis_server/libs/levelredis/level_redis.go
+++ goredis_server/libs/levelredis/level_redis.go
@@ -9,10 +9,10 @@ package levelredis
 对于string以外的复杂结构，还会有另外的字段，比如 hash 会有以_h开头的key，list会有_l开头的key
 
 2、leveldb存储原则
-因为整个设计都是为了海量存储的，所以所有支持的redis指令，都必须使用leveldb实现，不能消耗内存
+因为整个设计都是为了海量存储的，所以所有支持的redis指令，都必须基于leveldb实现，不能消耗内存
 必要的时候，会牺牲掉一些redis特性，比如list结构需要lindex的话，就必须放弃lrem和linsert
 
-同时会对使用场景进行一些取舍，比如zset要提供zcard的话，就需要每次操作后更新len，但增加的一次leveldb会降低zadd性能
+同时会对使用场景进行一些取舍，比如zset要提供zcard的话，就需要每次操作后更新len，但增加的一次leveldb操作会降低zadd性能
 因此对于hash、set这种很少取count的数据，放弃hlen、scard的性能（但也可以提供1000以内的枚举统计）,来提高hset/sadd的性能
 
 string
