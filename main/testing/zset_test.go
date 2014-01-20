177070cc6cd207798ff606c8b5813ef853561a3b
latermoon <latermoon@qq.com>
2014-01-20 17:46:31 +0800

    reflect testcase

--- main/testing/zset_test.go
+++ main/testing/zset_test.go
@@ -9,19 +9,45 @@ import (
 
 var host = ":1602"
 
+var pool = RedisPool(host)
+
+var lastT *testing.T
+
 func TestSortedSet(t *testing.T) {
-	conn := RedisPool(host).Get()
-	defer conn.Close()
+	lastT = t
 
 	key := "zz1"
-	reply, err := conn.Do("del", key)
-	CheckErr(t, err)
-	reply, err = conn.Do("zadd", key, "1", "a", "2", "b", "3", "c")
-	CheckErr(t, err)
-	CheckType(t, reply, reflect.Int64)
-	fmt.Println("->", reply.(int64))
+	checkCommand("del", key)
+	checkCommand("zadd", key, "1", "a", "2", "b", "3", "c")
+	checkCommand("zscore", key, "b")
+	checkCommand("zcard", key)
+	checkCommand("zrank", key, "c")
+	checkCommand("zrevrank", key, "c")
+	checkCommand("zrangebyscore", key, "0", "2")
 
-	reply, err = conn.Do("zscore", key, "b")
-	CheckErr(t, err)
-	fmt.Println(reply, reflect.TypeOf(reply))
+}
+
+// 发出指令，并打印返回值
+func checkCommand(cmdName string, args ...interface{}) {
+	conn := pool.Get()
+	defer conn.Close()
+
+	reply, err := conn.Do(cmdName, args...)
+	if err != nil {
+		lastT.Error(err)
+	}
+	fmt.Print("<--: ", cmdName)
+	for _, arg := range args {
+		fmt.Print(" ")
+		fmt.Print(arg)
+	}
+	fmt.Println()
+	fmt.Print("-->: ")
+	switch reply.(type) {
+	case []byte:
+		fmt.Print("\"", string(reply.([]byte)), "\"")
+	default:
+		fmt.Print(reply)
+	}
+	fmt.Printf(" (%s)\n\n", reflect.TypeOf(reply))
 }
