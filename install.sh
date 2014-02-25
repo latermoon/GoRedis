8c67974d62753ef228cba372e8a4b045e8479fda
latermoon <latermoon@qq.com>
2014-02-26 03:09:43 +0800

    remove go-rocksdb

--- install.sh
+++ install.sh
@@ -3,21 +3,22 @@
 echo "install goredis libs ..."
 
 go install "GoRedis/goredis"
-go install "GoRedis/libs/gorocks"
 go install "GoRedis/libs/counter"
 go install "GoRedis/libs/iotool"
 go install "GoRedis/libs/levelredis"
 go install "GoRedis/libs/lrucache"
 go install "GoRedis/libs/redis_tool"
-go install "GoRedis/libs/safelist"
 go install "GoRedis/libs/statlog"
 go install "GoRedis/libs/stdlog"
 go install "GoRedis/libs/uuid"
-go install "GoRedis/libs/queueprocess"
 go install "GoRedis/libs/rdb"
 go install "GoRedis/libs/rdb/crc64"
 go install "GoRedis/libs/geo"
-go install "GoRedis/goredis_server/slave"
+
+# 编译gorocks需要编译好rocksdb，并且配置环境变量
+# export CGO_CFLAGS="-I/home/download/rocksdb/include/"
+# export CGO_LDFLAGS="-L/home/download/rocksdb/ -lsnappy -lgflags -lz -lbz2"
+go install "GoRedis/libs/gorocks"
 
 echo "ok"
 
